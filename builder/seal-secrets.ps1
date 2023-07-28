e7670fc2a01c52cf363d161c885df4abe11b4c03
Martin Plessy <plessy.martin@outlook.com>
2023-07-28 12:07:27 +0100

    Warnings

--- builder/seal-secrets.ps1
+++ builder/seal-secrets.ps1
@@ -1,8 +1,11 @@
 Get-ChildItem -Path "$PSScriptRoot/*.yaml.secret" | ForEach-Object {
+	"# This file is generated, do not edit manually." `
+		> ($_.FullName -replace ".yaml.secret", ".yaml")
+
 	kubeseal `
 		--secret-file ($_.FullName) `
-		--sealed-secret-file ($_.FullName -replace ".yaml.secret", ".yaml") `
 		--allow-empty-data `
 		--cert "$PSScriptRoot/sealed-cert.pem" `
-		--format yaml
+		--format yaml `
+		>> ($_.FullName -replace ".yaml.secret", ".yaml")
 }
